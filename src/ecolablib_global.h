7094813d76edae8177121c98f1c72f842d6d6cf1
markuspg <markuspg@users.noreply.github.com>
2015-07-24 09:53:20 +0200

    Added and updated license snippets.

--- src/ecolablib_global.h
+++ src/ecolablib_global.h
@@ -1,20 +1,20 @@
 /*
  * Copyright 2015 Markus Prasser
  *
- * This file is part of Labcontrol.
+ * This file is part of EcoLabLib.
  *
- *  Labcontrol is free software: you can redistribute it and/or modify
+ *  EcoLabLib is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  Labcontrol is distributed in the hope that it will be useful,
+ *  EcoLabLib is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with Labcontrol.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with EcoLabLib.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef ECOLABLIB_GLOBAL_H
