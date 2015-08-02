edecce844d889d6a11ca01b988ba56e7b739eab6
markuspg <markuspg@users.noreply.github.com>
2015-08-02 18:36:11 +0200

    Documented and cleaned up 'ellzTree'.

--- src/session/ztree.h
+++ src/session/ztree.h
@@ -24,20 +24,39 @@
 
 #include <QObject>
 
+//! Starts and stores a z-Tree instance
+/*!
+  This class starts and stores a z-Tree instance. It passes all important options to z-Tree
+  and later keeps track of its finish.
+ */
 class ellzTree : public QObject
 {
     Q_OBJECT
 public:
+    //! 'ellzTree's constructor which will be called by 'ellSession'
+    /*!
+     * This constructor starts a new z-Tree instance with all given options.
+     * \param argSettingsStorage The 'ellSettingsStorage' containing all settings
+     * \param argZTreeDataTargetPath The path were z-Tree shall store all its data
+     * \param argZTreePort The port z-Tree shall listen on
+     * \param argZTreeVersionPath The path of the z-Tree version which shall be used
+     * \param argParent 'ellzTree's parent object
+     */
     explicit ellzTree( const ellSettingsStorage * const argSettingsStorage, const QString &argZTreeDataTargetPath,
                        const int &argZTreePort, const QString &argZTreeVersionPath, QObject *argParent = nullptr );
 
 signals:
+    /*!
+     * \brief This signal is emitted if z-Tree closes
+     * \param argExitCode The exit code of the z-Tree process
+     * \param argExitStatus The exit status of the z-Tree process
+     */
     void zTreeClosed( const int &argExitCode, const QProcess::ExitStatus &argExitStatus );
 
 public slots:
 
 private:
-    QProcess ztreeInstance;
+    QProcess ztreeInstance; //! Stores the actual z-Tree process instance
 };
 
 #endif // ZTREE_H
