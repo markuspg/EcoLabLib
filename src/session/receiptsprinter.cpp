cbd6a91b0310bd6dcb7cf754e139a2ecba56b090
markuspg <markuspg@users.noreply.github.com>
2015-08-02 21:28:14 +0200

    Documented and cleaned up 'ellReceiptsPrinter'.

--- src/session/receiptsprinter.cpp
+++ src/session/receiptsprinter.cpp
@@ -14,52 +14,40 @@ void ellReceiptsPrinter::run() {
     QStringList arguments;
     arguments << "-interaction" << "batchmode" << QString{ *dateString + ".tex" };
 
-    QProcess *process = nullptr;
-    process = new QProcess{};
-    QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
-    process->setProcessEnvironment( env );
+    std::unique_ptr< QProcess > process{ new QProcess{} };
+    process->setProcessEnvironment( *settingsStorage->processEnvironment );
     process->setWorkingDirectory( *zTreeDataTargetPath );
     process->start( *settingsStorage->latexCommand, arguments );
     if( !process->waitForFinished( processTimeOut ) ) {
         true;
-        delete process;
-        process = nullptr;
         return;
     }
-    delete process;
-    process = nullptr;
 
+    process.reset( new QProcess{} );
     // Convert the dvi file to postscript
     arguments.clear();
     arguments << "-q*" << "-o" << QString{ *dateString + ".ps" } << QString{ *dateString + ".dvi" };
 
-    process = new QProcess{};
-    process->setProcessEnvironment( env );
+    process->setProcessEnvironment( *settingsStorage->processEnvironment );
     process->setWorkingDirectory( *zTreeDataTargetPath );
     process->start( *settingsStorage->dvipsCommand, arguments );
     if( !process->waitForFinished( processTimeOut ) ) {
         true;
-        delete process;
-        process = nullptr;
         return;
     }
-    delete process;
-    process = nullptr;
 
     // Print the postscript file
     if ( settingsStorage->lprCommand ) {
         arguments.clear();
         arguments << QString{ *zTreeDataTargetPath + "/" + *dateString + ".ps" };
 
-        process = new QProcess{};
-        process->setProcessEnvironment( env );
+        process.reset( new QProcess{} );
+        process->setProcessEnvironment( *settingsStorage->processEnvironment );
         process->setWorkingDirectory( *zTreeDataTargetPath );
         process->start( *settingsStorage->lprCommand, arguments );
         if( !process->waitForFinished( processTimeOut ) ) {
             true;
         }
-        delete process;
-        process = nullptr;
     }
 
     // Convert the postscript file to pdf
@@ -68,28 +56,24 @@ void ellReceiptsPrinter::run() {
         arguments << QString{ *zTreeDataTargetPath + "/" + *dateString + ".ps" }
                   << QString{ *zTreeDataTargetPath + "/" + *dateString + ".pdf" };
 
-        process = new QProcess{};
-        process->setProcessEnvironment( env );
+        process.reset( new QProcess{} );
+        process->setProcessEnvironment( *settingsStorage->processEnvironment );
         process->setWorkingDirectory( *zTreeDataTargetPath );
         process->start( *settingsStorage->ps2pdfCommand, arguments );
         if( !process->waitForFinished( processTimeOut ) ) {
             true;
         }
-        delete process;
-        process = nullptr;
+    }
 
-        // Show the postscript file if the conversion succeeded
-        if ( settingsStorage->postscriptViewer ) {
-            arguments.clear();
-            arguments << QString{ *zTreeDataTargetPath + "/" + *dateString + ".ps" };
+    // Show the postscript file if the conversion succeeded
+    if ( settingsStorage->postscriptViewer ) {
+        arguments.clear();
+        arguments << QString{ *zTreeDataTargetPath + "/" + *dateString + ".ps" };
 
-            process = new QProcess{};
-            process->setProcessEnvironment( env );
-            process->setWorkingDirectory( *zTreeDataTargetPath );
-            process->startDetached( *settingsStorage->postscriptViewer, arguments );
-            delete process;
-            process = nullptr;
-        }
+        process.reset( new QProcess{} );
+        process->setProcessEnvironment( *settingsStorage->processEnvironment );
+        process->setWorkingDirectory( *zTreeDataTargetPath );
+        process->startDetached( *settingsStorage->postscriptViewer, arguments );
     }
 
     // Clean up the zTree working path
@@ -100,15 +84,13 @@ void ellReceiptsPrinter::run() {
                   << QString{ *zTreeDataTargetPath + "/" + *dateString + ".log" }
                   << QString{ *zTreeDataTargetPath + "/" + *dateString + ".tex" };
 
-        process = new QProcess{};
-        process->setProcessEnvironment( env );
+        process.reset( new QProcess{} );
+        process->setProcessEnvironment( *settingsStorage->processEnvironment );
         process->setWorkingDirectory( *zTreeDataTargetPath );
         process->start( *settingsStorage->rmCommand, arguments);
         if( !process->waitForFinished( processTimeOut ) ) {
             true;
         }
-        delete process;
-        process = nullptr;
       }
 
     emit PrintingFinished();
--- src/session/receiptsprinter.h
+++ src/session/receiptsprinter.h
@@ -7,14 +7,28 @@
 #include <QStringList>
 #include <QThread>
 
+//! Prints the created receipts LaTeX file in another thread
+/*!
+  This class is used to print the receipts LaTeX file created by 'ellReceiptsCreator'
+  in another thread to prevent the user interface from freezing.
+ */
 class ellReceiptsPrinter : public QThread
 {
     Q_OBJECT
 public:
+    //! 'ellReceiptsPrinter's constructor which will be called by 'ellReceiptsCreator'
+    /*!
+       This constructor does nothing else than initializing this class' attributes with the passed ones.
+       \param argDateString The date string of the session to be printed ("yyMMdd_hhmm")
+       \param argzTreeDataTargetPath The path were the data of this zTree instance's session will be saved
+       \param argSettingsStorage The 'ellSettingsStorage' containing all settings
+       \param argParent 'ellReceiptsPrinter's parent object
+     */
     explicit ellReceiptsPrinter( const QString * const argDateString, const QString * const argzTreeDataTargetPath,
                                  const ellSettingsStorage * const argSettingsStorage, QObject *argParent = nullptr );
 
 signals:
+    //! This signal becomes emitted when the printing has finished
     void PrintingFinished();
 
 public slots:
@@ -22,9 +36,10 @@ public slots:
 private:
     const QString * const dateString = nullptr;             //! The date string of the session to be printed ("YYMMDD_hhmm")
     const int processTimeOut = 15000;                       //! The maximum runtime which will be granted to a started process
-    const ellSettingsStorage * const settingsStorage;
+    const ellSettingsStorage * const settingsStorage;       //! Contains all external settings
     const QString * const zTreeDataTargetPath = nullptr;    //! The path were the data of this zTree instance's session will be saved
 
+    //! This function will print the receipts and will be executed in a separate thread
     void run() Q_DECL_OVERRIDE;
 };
 
