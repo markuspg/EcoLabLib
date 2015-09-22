9647dacb3f2f0d404e105975fa4c675177721a8b
markuspg <markuspg@users.noreply.github.com>
2015-09-22 18:23:32 +0200

    Added some new scripts.

--- scripts/ActivateCalculator.sh
+++ scripts/ActivateCalculator.sh
@@ -23,26 +23,8 @@
 
 for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18; do
 echo "client$i"
-ssh -T expuser@client$i <<THEEND
-DISPLAY=:0.0 xdotool mousemove 1176 308
-sleep 0.2
-DISPLAY=:0.0 xdotool click 1
-sleep 0.2
-DISPLAY=:0.0 xdotool mousemove 780 60
-sleep 0.2
-DISPLAY=:0.0 xdotool click 1
-sleep 0.2
-DISPLAY=:0.0 xdotool type "client$i"
-sleep 0.2
-DISPLAY=:0.0 xdotool mousemove 812 86
-sleep 0.2
-DISPLAY=:0.0 xdotool click 1
-sleep 0.2
-DISPLAY=:0.0 xdotool type "client$i"
-sleep 0.2
-DISPLAY=:0.0 xdotool mousemove 1024 124
-sleep 0.2
-DISPLAY=:0.0 xdotool click 1
-sleep 0.2
+ssh -T ewfuser@client$i <<THEEND
+ln -s /home/ewfuser/.wine/drive_c/Program\ Files/SpeedCrunch/speedcrunch.exe /home/ewfuser/.wine/drive_c/windows/system32/CALC.exe
+ln -s /home/ewfuser/.wine/drive_c/Program\ Files/SpeedCrunch/speedcrunch.exe /home/ewfuser/.wine/drive_c/windows/system32/CALC.EXE
 THEEND
 done
--- scripts/DeactivateCalculator.py
+++ scripts/DeactivateCalculator.py
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env python3
 
 ##############################################################################
 ##
@@ -21,28 +21,9 @@
 ##
 ##############################################################################
 
-for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18; do
-echo "client$i"
-ssh -T expuser@client$i <<THEEND
-DISPLAY=:0.0 xdotool mousemove 1176 308
-sleep 0.2
-DISPLAY=:0.0 xdotool click 1
-sleep 0.2
-DISPLAY=:0.0 xdotool mousemove 780 60
-sleep 0.2
-DISPLAY=:0.0 xdotool click 1
-sleep 0.2
-DISPLAY=:0.0 xdotool type "client$i"
-sleep 0.2
-DISPLAY=:0.0 xdotool mousemove 812 86
-sleep 0.2
-DISPLAY=:0.0 xdotool click 1
-sleep 0.2
-DISPLAY=:0.0 xdotool type "client$i"
-sleep 0.2
-DISPLAY=:0.0 xdotool mousemove 1024 124
-sleep 0.2
-DISPLAY=:0.0 xdotool click 1
-sleep 0.2
-THEEND
-done
+import subprocess
+nummern = ["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18"] # hilfsarray mit den Clientnummern inkl. führender Nullen als String.
+
+for i in nummern:
+    print( i )
+    subprocess.call("ssh ewfuser@client" + i + " rm -rv /home/ewfuser/.wine/drive_c/windows/system32/CALC.* & exit",shell=True,)
